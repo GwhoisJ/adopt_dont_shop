3758178ae89e93f5bd15e95f8897c1c3dd333adf
Gregory Fischer <gwhoisj@gmail.com>
2021-10-23 12:03:14 -0400

    Feat: routes and controller set up for new

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,4 +2,30 @@ class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
   end
+
+  def new
+    @application = Application.find(params[:id])
+  end
+
+  def create
+    address = params[:street_address] + " ," + params[:city] + " ," + params[:state] + " " + params[:zip_code]
+
+    application = Application.new({
+       name: params[:name],
+       address: address,
+       description: params[:description],
+       status: "In Progress"
+       })
+    
+    application.save
+
+    redirect_to "/applications/#{application.id}"
+  end
+
+  # private
+
+  # def application_params
+  #   # address = params[:street_address] + " ," + params[:city] + " ," + params[:state] + " " + params[:zip_code]
+  #   params.permit(:id, :name, :street_address, :city, :state, :zip_code, :description)
+  # end
 end
