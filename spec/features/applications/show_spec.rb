73e4186d49c4012d70a05d785f30ae4ac367a52a
Gregory Fischer <gwhoisj@gmail.com>
2021-10-24 15:18:32 -0400

    Refactor: fix test functionality

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -34,15 +34,34 @@ RSpec.describe 'show page for applications' do
     pet_2 = Pet.create(adoptable: true, age: 3, breed: 'domestic pig', name: 'Babe', shelter_id: shelter.id)
     pet_3 = Pet.create(adoptable: true, age: 4, breed: 'chihuahua', name: 'Elle', shelter_id: shelter.id)
 
-    visit "/pets"
+    visit "/applications/#{@application.id}"
 
-    fill_in 'search', with: "Ba"
-    click_on("Search")
+    fill_in 'Add a Pet to this Application', with: "Ba"
+    click_on("Submit")
 
     expect(page).to have_content(pet_1.name)
     expect(page).to have_content(pet_2.name)
     expect(page).to_not have_content(pet_3.name)
   end
 
-  it 'has an adopt a pet option'
+  it 'has an adopt a pet option' do
+    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    pet_1 = Pet.create(adoptable: true, age: 7, breed: 'sphynx', name: 'Bare-y Manilow', shelter_id: shelter.id)
+    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'domestic pig', name: 'Babe', shelter_id: shelter.id)
+    pet_3 = Pet.create(adoptable: true, age: 4, breed: 'chihuahua', name: 'Elle', shelter_id: shelter.id)
+
+    visit "/applications/#{@application.id}"
+
+    fill_in 'Add a Pet to this Application', with: "Ba"
+    click_on("Submit")
+
+    within("#pet-0") do
+      expect(page).to have_button("Adopt this Pet")
+      click_on("Adopt this Pet")
+    end
+
+    expect(current_path).to eq("/applications/#{@application.id}")
+
+    expect(@application.pets).to include(pet_1)
+  end
 end
